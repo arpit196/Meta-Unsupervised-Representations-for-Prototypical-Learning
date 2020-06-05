5aff042415ea2ec8fb487c8ee17eb82494ebae9c
arpit196 <30366755+arpit196@users.noreply.github.com>
2020-06-05 14:05:16 +0530

    Update prototypical.py

--- prototf/models/prototypical.py
+++ prototf/models/prototypical.py
@@ -56,11 +56,6 @@ class Prototypical(Model):
             tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same'),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.ReLU(),
-            tf.keras.layers.MaxPool2D((2, 2)),
-
-            tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same'),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.ReLU(),
             tf.keras.layers.MaxPool2D((2, 2)), Flatten()]
         )
 
