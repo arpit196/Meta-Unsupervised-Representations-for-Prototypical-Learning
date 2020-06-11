6e512cce4bc70eb69a8b8b6bb22b323329efa491
arpit196 <30366755+arpit196@users.noreply.github.com>
2020-06-11 16:49:20 +0530

    Update prototypical.py

--- prototf/models/prototypical.py
+++ prototf/models/prototypical.py
@@ -146,7 +146,7 @@ class Prototypical(Model):
         log_p_y = tf.nn.log_softmax(-dists, axis=-1)
         log_p_y = tf.reshape(log_p_y, [n_class, n_query, -1])
         
-        loss = -tf.reduce_mean(tf.reshape(tf.reduce_sum(tf.multiply(y_onehot, log_p_y), axis=-1), [-1])) + tf.norm(self.W1, ord=1)#+ uns_loss 
+        loss = -tf.reduce_mean(tf.reshape(tf.reduce_sum(tf.multiply(y_onehot, log_p_y), axis=-1), [-1])) + tf.norm(self.W, ord=1)#+ uns_loss 
         eq = tf.cast(tf.equal(
             tf.cast(tf.argmax(log_p_y, axis=-1), tf.int32), 
             tf.cast(y, tf.int32)), tf.float32)
