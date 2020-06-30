e9cd097e1d6145cbbfa0fb4133d8ba1bc06ccb82
arpit196 <30366755+arpit196@users.noreply.github.com>
2020-06-30 15:30:36 +0530

    Update prototypical.py

--- prototf/models/prototypical.py
+++ prototf/models/prototypical.py
@@ -129,7 +129,7 @@ class Prototypical(Model):
         self.uns_enc = tf.keras.Sequential()
         self.uns_enc.add(self.l14)
         self.uns_enc.add(tf.keras.layers.ReLU())
-        self.uns_enc.add(tf.keras.layers.Dense(150))
+        self.uns_enc.add(tf.keras.layers.Dense(100))
         '''
         self.decoder = tf.keras.Sequential()
         self.decoder.add(tf.keras.layers.Reshape(60,30,30))
@@ -183,8 +183,6 @@ class Prototypical(Model):
         z_uns = self.uns_enc(z)
         print("zshape")
         print(z.shape)
-        print("l13")
-        print(self.l13)
         
         z1 = tf.reshape(z_uns[:n_class*n_support],[n_class, n_support, z_uns.shape[-1]])
         
@@ -253,7 +251,7 @@ class Prototypical(Model):
         log_p_y = tf.nn.log_softmax(-dists, axis=-1)
         log_p_y = tf.reshape(log_p_y, [n_class, n_query, -1])
         
-        loss = -tf.reduce_mean(tf.reshape(tf.reduce_sum(tf.multiply(y_onehot, log_p_y), axis=-1), [-1])) + uns_loss 
+        loss = -tf.reduce_mean(tf.reshape(tf.reduce_sum(tf.multiply(y_onehot, log_p_y), axis=-1), [-1])) - uns_loss 
         eq = tf.cast(tf.equal(
             tf.cast(tf.argmax(log_p_y, axis=-1), tf.int32), 
             tf.cast(y, tf.int32)), tf.float32)
